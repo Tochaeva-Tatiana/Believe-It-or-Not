9a4bb13ba5e1a9d20dbc4da759bf099a69a80b93
Точаева Татьяна <t.tochaeva@yandex.ru>
2026-06-17 19:48:48 +0300

    feat(server): implement card and deck classes

---
+++ believe/common/__init__.py
@@ -0,0 +1,40 @@
+"""Common constants for Believe-It-or-Not."""
+
+HOST = "localhost"
+PORT = 1337
+
+MIN_PLAYERS = 2
+MAX_PLAYERS = 4
+
+INVITATION_TIMEOUT = 10
+TURN_MESSAGE_DELAY = 1
+
+SUITS = (
+    "♣️",
+    "♦️",
+    "♥️",
+    "♠️",
+)
+
+RANKS = (
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "D",
+    "K",
+    "A",
+)
+
+DECLARABLE_RANKS = (
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "D",
+    "K",
+)
