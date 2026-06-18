ad89208be7acb3eca8b7b456deaeb848e682dba9
Точаева Татьяна <t.tochaeva@yandex.ru>
2026-06-18 03:59:01 +0300

    docs: configure Sphinx documentation

---
+++ doc/API.rst
@@ -0,0 +1,28 @@
+Software interface
+=====================
+
+Client
+------
+
+.. automodule:: believe.client.__main__
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Server
+------
+
+.. automodule:: believe.server.__main__
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Game logic
+--------------
+
+.. automodule:: believe.server.game
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
---
+++ doc/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
+
+project = 'Believe-It-or-Not'
+copyright = '2026, Strelkina Irina, Tochaeva Tatiana'
+author = 'Strelkina Irina, Tochaeva Tatiana'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
